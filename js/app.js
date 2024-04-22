cb76710bb2d96581e7000ecb4774d7d8425989ce
alyxandre-beniahia <alyxandre.beniahia@laplateforme.io>
2024-04-22 17:46:22 +0200

    added popular tv shows fetch, id and title search

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 const apiKey = "8c4b867188ee47a1d4e40854b27391ec";
 
 /**
+ * Retrieves a guest session token from the MovieDB API.
+ * @returns {Promise<Object>} The guest session token data received from the MovieDB API.
+ * @throws {Error} If there is an HTTP error during the request.
+ */
+const getGuestToken = async () => {
+  const url = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+/**
  * Fetches movie data from an API using an API key.
  * @returns {Promise<Array>} An array of movie objects.
  * @throws {Error} If there is an HTTP error or an error occurs during the fetch.
@@ -69,20 +89,34 @@ const fetchMoviesById = async (id) => {
     return [];
   }
 };
-fetchMoviesById(22)
-  .then((movie) => {})
-  .catch((error) => {
-    console.error("Error occurred: ", error);
-  });
 
 /**
- * Retrieves a guest session token from the MovieDB API.
- * @returns {Promise<Object>} The guest session token data received from the MovieDB API.
- * @throws {Error} If there is an HTTP error during the request.
+ * Fetches tv shows data from an API using an API key.
+ * @returns {Promise<Array>} An array of tv shows objects.
+ * @throws {Error} If there is an HTTP error or an error occurs during the fetch.
  */
-const getGuestToken = async () => {
-  const url = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${apiKey}`;
+const fetchApiTvs = async () => {
+  const url = `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&api_key=${apiKey}&sort_by=popularity.desc`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
 
+/**
+ * Fetches Tv shows data from an API using the provided Tvs ID.
+ * @param {number} id - The ID of the tv show to fetch.
+ * @returns {Promise<object>} - The fetched tv show data as an object.
+ */
+const fetchTvsById = async (id) => {
+  const url = `https://api.themoviedb.org/3/tv/${id}?language=en-US&api_key=${apiKey}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -90,16 +124,28 @@ const getGuestToken = async () => {
     }
     const data = await response.json();
     return data;
-  } catch (error) {
+  } catch {
     console.error("Error fetching data: ", error);
     return [];
   }
 };
 
-getGuestToken()
-  .then((data) => {
-    console.log(data.guest_session_id);
-  })
-  .catch((error) => {
-    console.error("Error occurred: ", error);
-  });
+/**
+ * Fetches tv show data from an API based on a given search term.
+ * @param {string} searchTerm - The term to search for tv shows.
+ * @returns {Promise<Array>} - An array of tv shows objects that match the search term. If there is an error during the API request, an empty array is returned.
+ */
+const fetchTvsByTitle = async (searchTerm) => {
+  const url = `https://api.themoviedb.org/3/search/tv?query=${searchTerm}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
