bc159e2ed6116f362d014433d4a94adb1572ae3f
Liron Saiman <163527712+lsDevPlateforme@users.noreply.github.com>
2024-04-25 20:16:02 +0200

    add trailer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-const fetchApiMovie = async (movieId) => {
-  const apiKey = "39787069cfe20f504fbbf4f3df331d89";
-  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
+const apiKey = "8c4b867188ee47a1d4e40854b27391ec";
+const baseImgUrl = "https://image.tmdb.org/t/p/w400";
+
+/**
+ * Retrieves a guest session token from the MovieDB API.
+ * @returns {Promise<Object>} The guest session token data received from the MovieDB API.
+ * @throws {Error} If there is an HTTP error during the request.
+ */
+const getGuestToken = async () => {
+  const url = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${apiKey}`;
 
   try {
     const response = await fetch(url);
@@ -8,10 +15,204 @@ const fetchApiMovie = async (movieId) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+/**
+ * Fetches movie data from an API using an API key.
+ * @returns {Promise<Array>} An array of movie objects.
+ * @throws {Error} If there is an HTTP error or an error occurs during the fetch.
+ */
+
+const fetchMovieGenres = async () => {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?language=fr&api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.genres;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
   }
 };
 
-fetchApiMovie(11);
+const fetchApiMovies = async (page = 1) => {
+  const url = `https://api.themoviedb.org/3/discover/movie?language=fr&api_key=${apiKey}&page=${page}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.results.push(data.page);
+    data.results.push(data.total_pages);
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+const fetchUpcomingMovies = async () => {
+  const url = `https://api.themoviedb.org/3/movie/upcoming?language=fr&api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+const fetchVideoMovies = async () => {
+  const movies = await fetchUpcomingMovies();
+  console.log(movies);
+  const id = movies[0].id;
+  const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=fr-FR
+  `;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+/**
+ * Fetches movie data from an API based on a given search term.
+ * @param {string} searchTerm - The term to search for movies.
+ * @returns {Promise<Array>} - An array of movie objects that match the search term. If there is an error during the API request, an empty array is returned.
+ */
+const fetchMoviesByTitle = async (searchTerm) => {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=fr&page=1&api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+/**
+ * Fetches movie data from an API using the provided movie ID.
+ * @param {number} id - The ID of the movie to fetch.
+ * @returns {Promise<object>} - The fetched movie data as an object.
+ */
+const fetchMoviesById = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=fr&api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+/**
+ * Fetches tv shows data from an API using an API key.
+ * @returns {Promise<Array>} An array of tv shows objects.
+ * @throws {Error} If there is an HTTP error or an error occurs during the fetch.
+ */
+
+const fetchTvsGenres = async () => {
+  const url = `https://api.themoviedb.org/3/genre/tv/list?language=fr&api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.genres;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+const fetchApiTvs = async (page = 1) => {
+  const url = `https://api.themoviedb.org/3/discover/tv?include_adult=false&language=fr&page=${page}&api_key=${apiKey}&sort_by=vote_count.desc`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.results.push(data.page);
+    data.results.push(data.total_pages);
+    return data.results;
+  } catch {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+/**
+ * Fetches Tv shows data from an API using the provided Tvs ID.
+ * @param {number} id - The ID of the tv show to fetch.
+ * @returns {Promise<object>} - The fetched tv show data as an object.
+ */
+const fetchTvsById = async (id) => {
+  const url = `https://api.themoviedb.org/3/tv/${id}?language=fr&api_key=${apiKey}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
+
+/**
+ * Fetches tv show data from an API based on a given search term.
+ * @param {string} searchTerm - The term to search for tv shows.
+ * @returns {Promise<Array>} - An array of tv shows objects that match the search term. If there is an error during the API request, an empty array is returned.
+ */
+const fetchTvsByTitle = async (searchTerm) => {
+  const url = `https://api.themoviedb.org/3/search/tv?query=${searchTerm}&include_adult=false&language=fr&page=1&api_key=${apiKey}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return [];
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,75 +1,224 @@
 /*=============== SHOW MENU ===============*/
-const nav = document.getElementById('nav'),
-      headerMenu = document.getElementById('header-menu'),
-      navClose = document.getElementById('nav-close')
+const nav = document.getElementById("nav"),
+  headerMenu = document.getElementById("header-menu"),
+  navClose = document.getElementById("nav-close");
 
 /* Menu show */
-if(headerMenu){
-   headerMenu.addEventListener('click', () =>{
-      nav.classList.add('show-menu')
-   })
+if (headerMenu) {
+  headerMenu.addEventListener("click", () => {
+    nav.classList.add("show-menu");
+  });
 }
 
 /* Menu hidden */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-      nav.classList.remove('show-menu')
-   })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    nav.classList.remove("show-menu");
+  });
 }
+/*=============== SWIPER BANNER ===============*/
 
 /*=============== SWIPER MOVIE ===============*/
-let swiperMovie = new Swiper('.movie__swiper', {
-   loop: true,
-   grabCursor: true,
-   slidesPerView: 2,
-   spaceBetween: 24,
+let swiperMovies = new Swiper(".movie__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 2,
+  spaceBetween: 24,
+  breakpoints: {
+    440: {
+      slidesPerView: "auto",
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+/*=============== SWIPER TvShows ===============*/
 
-   breakpoints:{
-      440: {
-         slidesPerView: 'auto',
-      },
-      768: {
-         slidesPerView: 4,
-      },
-      1200: {
-         slidesPerView: 5,
-      },
-   },
-})
+let swiperTvShows = new Swiper(".tvs__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 2,
+  spaceBetween: 24,
+  breakpoints: {
+    440: {
+      slidesPerView: "auto",
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
 
 /*=============== SWIPER NEW ===============*/
-let swiperNew = new Swiper('.new__swiper', {
-   loop: true,
-   grabCursor: true,
-   centeredSlides: true,
-   slidesPerView: 2,
+// let swiperNew = new Swiper(".new__swiper", {
+//   loop: true,
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: 2,
 
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
 
-   breakpoints:{
-      440: {
-         centeredSlides: false,
-         slidesPerView: 'auto',
-      },
-      768: {
-         centeredSlides: false,
-         slidesPerView: 4,
-      },
-      1200: {
-         centeredSlides: false,
-         slidesPerView: 5,
-      },
-   },
-})
+//   breakpoints: {
+//     440: {
+//       centeredSlides: false,
+//       slidesPerView: "auto",
+//     },
+//     768: {
+//       centeredSlides: false,
+//       slidesPerView: 4,
+//     },
+//     1200: {
+//       centeredSlides: false,
+//       slidesPerView: 5,
+//     },
+//   },
+// });
 
 /*=============== ADD BLUR HEADER ===============*/
-const blurHeader = () =>{
-   const header = document.getElementById('header')
-   // Add a class if the bottom offset is greater than 50 of the viewport
-   this.scrollY >= 50 ? header.classList.add('blur-header') 
-                      : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
+
+const movieSwiperContainer = document.getElementById("movie_swiper_container");
+const tvsSwiperContainer = document.getElementById("tvs_swiper_container");
+const bannerContainer = document.getElementById("banner");
+let movieGenres = [];
+let tvsGenres = [];
+
+fetchMovieGenres()
+  .then((genres) => {
+    const genresArray = [];
+    for (let i = 0; i < genres.length; i++) {
+      genresArray.push(genres[i]);
+    }
+    movieGenres = genresArray;
+  })
+  .catch((error) => {
+    console.error("Error occurred: ", error);
+  });
+
+fetchApiMovies()
+  .then((movies) => {
+    movies.pop();
+    movies.pop();
+    movies.forEach((movie) => {
+      console.log(movie);
+      // Create elements
+      const movieArticle = document.createElement("article");
+      movieArticle.className = "card__article swiper-slide";
+
+      const movieLink = document.createElement("a");
+      movieLink.classList.add("card__link");
+
+      const movieImg = document.createElement("img");
+      movieImg.classList.add("card__img");
+      movieImg.src = `${baseImgUrl}${movie.backdrop_path}`;
+      movieImg.alt = `${movie.title} image`;
+
+      const movieShadow = document.createElement("div");
+      movieShadow.classList.add("card__shadow");
+
+      const movieData = document.createElement("div");
+      movieData.classList.add("card__data");
+
+      const movieTitle = document.createElement("h3");
+      movieTitle.classList.add("card__name");
+      movieTitle.textContent = movie.title;
+
+      const movieLikeIcon = document.createElement("i");
+      movieLikeIcon.classList.add("ri-heart-3-line", "card__like");
+
+      // Append elements
+      movieData.appendChild(movieTitle);
+
+      movieLink.appendChild(movieImg);
+      movieLink.appendChild(movieShadow);
+      movieLink.appendChild(movieData);
+      movieLink.appendChild(movieLikeIcon);
+
+      movieArticle.appendChild(movieLink);
+
+      movieSwiperContainer.appendChild(movieArticle);
+    });
+  })
+  .catch((error) => {
+    console.error("Error occurred: ", error);
+  });
+
+fetchVideoMovies().then((video) => {
+  console.log(video);
+});
+
+fetchTvsGenres()
+  .then((genres) => {
+    const genresArray = [];
+    for (let i = 0; i < genres.length; i++) {
+      genresArray.push(genres[i]);
+    }
+    tvsGenres = genresArray;
+  })
+  .catch((error) => {
+    console.error("Error occurred: ", error);
+  });
+
+fetchApiTvs()
+  .then((Tvs) => {
+    Tvs.pop();
+    Tvs.pop();
+    Tvs.forEach((tvs, index) => {
+      // Create elements
+      const tvsArticle = document.createElement("article");
+      tvsArticle.className = "new__card card__article swiper-slide";
+
+      const tvsLink = document.createElement("a");
+      tvsLink.classList.add("card__link");
+
+      const tvsImg = document.createElement("img");
+      tvsImg.classList.add("card__img");
+      tvsImg.src = `${baseImgUrl}${tvs.backdrop_path}`;
+      tvsImg.alt = `${tvs.title} image`;
+
+      const tvsShadow = document.createElement("div");
+      tvsShadow.classList.add("card__shadow");
+
+      const tvsData = document.createElement("div");
+      tvsData.className = "new__data card__data";
+
+      const tvsTitle = document.createElement("h3");
+      tvsTitle.classList.add("card__name");
+      tvsTitle.textContent = tvs.name;
+
+      const tvsLikeIcon = document.createElement("i");
+      tvsLikeIcon.classList.add("ri-heart-3-line", "card__like");
+      // console.log(tvsCategory);
+
+      // Append elements
+      tvsData.appendChild(tvsTitle);
+
+      tvsLink.appendChild(tvsImg);
+      tvsLink.appendChild(tvsShadow);
+      tvsLink.appendChild(tvsData);
+      tvsLink.appendChild(tvsLikeIcon);
+
+      tvsArticle.appendChild(tvsLink);
+
+      tvsSwiperContainer.appendChild(tvsArticle);
+    });
+  })
+  .catch((error) => {
+    console.error("Error occurred: ", error);
+  });
