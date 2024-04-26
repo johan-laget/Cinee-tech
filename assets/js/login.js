89dafe0eee4642e4a8bd521421b8cb51a84c5426
johan-laget <johan.laget@laplateforme.io>
2024-04-26 15:39:16 +0200

    push login- main

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,26 +17,3 @@ document.getElementById("logInForm").addEventListener("submit", function (event)
         alert("Log in failed");
     }
 });
-
-const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-const authButtons = document.getElementById("auth-buttons");
-const profileButtons = document.getElementById("profile-buttons");
-
-const logOutBtn = document.getElementById("logOutBtn");
-
-if (currentUser) {
-   logOutBtn.addEventListener("click", function() {
-    localStorage.removeItem("currentUser");
-    window.location.href = "./index.html";
-    });
-}
-
-if (currentUser) {
-    authButtons.style.display = "none";
-    profileButtons.style.display = "block";
-    const profileButton = document.getElementById("profile-button");
-    profileButton.value = currentUser.pseudo;
-} else {
-    authButtons.style.display = "block";
-    profileButtons.style.display = "none";
-}
